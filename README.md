a5ab5432a31433e7fb6c06776e2bb74c4431823b
FlitSneak <870295595@qq.com>
2020-11-11 19:39:17 +0800

    完成商品分类的递归树查询，完善网关请求转发功能。

--- README.md
+++ README.md
@@ -1,23 +1,11 @@
 # 谷粒商城gulimall
 
 #### 介绍
-基于SpringAlibaba的分布式微服务商城
+此为尚硅谷开放的一个微服务项目。总体来说这个项目比较完备，涉及到了分布式事务，集群部署以及运维的流行技术K8S和Jenkins。
 
-#### 软件架构
-软件架构说明
+总体设计来看，服务注册和配置中心为SpringAlibaba Nacos，从功能和广度还有便捷度来看都要比Eureka要强的多，网关为SpringCloud Gateway zuul已经去世，zuul2还在挂机。Ribbon实现负载均衡，同时Sentinel实现服务的熔断降级这里取代了Hystrix豪猪哥。请求先行到SpringSecurit和OAuth2.0进行认证，认证实现第三方登录和单点登录，涉及到远程服务调用，远程服务调用框架为OpenFeign。集群的日志收集过滤全文搜索引擎为ELK，ElasticSearch+LogStash+Kibana.服务追踪可视化为Sleuth+Zipkin.对象存储采用阿里云OSS，数据库持久层为Mysql，缓存为redis集群。消息队列采用RabbitMQ。后端运维采用K8S+Jenkins实现CI/CD。
 
-
-#### 安装教程
-
-1.  xxxx
-2.  xxxx
-3.  xxxx
-
-#### 使用说明
-
-1.  xxxx
-2.  xxxx
-3.  xxxx
+总体划分服务为认证服务+商品微服务+用户服务+存储服务+购物车服务+订单服务+支付服务+优惠服务+秒杀服务+全文检索服务。
 
 #### 参与贡献
 
@@ -25,13 +13,3 @@
 2.  新建 Feat_xxx 分支
 3.  提交代码
 4.  新建 Pull Request
-
-
-#### 特技
-
-1.  使用 Readme\_XXX.md 来支持不同的语言，例如 Readme\_en.md, Readme\_zh.md
-2.  Gitee 官方博客 [blog.gitee.com](https://blog.gitee.com)
-3.  你可以 [https://gitee.com/explore](https://gitee.com/explore) 这个地址来了解 Gitee 上的优秀开源项目
-4.  [GVP](https://gitee.com/gvp) 全称是 Gitee 最有价值开源项目，是综合评定出的优秀开源项目
-5.  Gitee 官方提供的使用手册 [https://gitee.com/help](https://gitee.com/help)
-6.  Gitee 封面人物是一档用来展示 Gitee 会员风采的栏目 [https://gitee.com/gitee-stars/](https://gitee.com/gitee-stars/)
--- mall-product/src/main/java/com/flitsneak/mall/product/service/impl/CategoryServiceImpl.java
+++ mall-product/src/main/java/com/flitsneak/mall/product/service/impl/CategoryServiceImpl.java
@@ -1,7 +1,13 @@
 package com.flitsneak.mall.product.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.conditions.segments.MergeSegments;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -26,4 +32,43 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
         return new PageUtils(page);
     }
 
+    @Override
+    public List<CategoryEntity> listWithTree() {
+        //1、查询所有分类
+       List<CategoryEntity> entities = baseMapper.selectList(null);
+
+       //2、组装成父子的树形结构
+        //2、1 找到所有的一级分类
+        List<CategoryEntity> level1Menus = entities.stream().filter(categoryEntity ->
+                categoryEntity.getParentCid() == 0
+        ).map((menu)->{//每一个一级分类都映射到map中。
+            menu.setChildren(getChildren(menu,entities));//一级分类追加list<Category>
+            return menu;
+        }).sorted((menu1,menu2)->{
+            return (menu1.getSort()==null?0:menu1.getSort()) - (menu2.getSort()==null?0:menu2.getSort());
+        }).collect(Collectors.toList());
+
+        return level1Menus;
+    }
+
+    @Override
+    public void removeMenuByIds(List<Long> asList) {
+        //TODO 检查当前菜单是否被别人引用
+        baseMapper.deleteBatchIds(asList);
+    }
+
+    private List<CategoryEntity> getChildren(CategoryEntity parent,List<CategoryEntity> entities){
+
+        List<CategoryEntity> children = entities.stream()
+                .filter(categoryEntity -> categoryEntity.getParentCid()==parent.getCatId())
+                .map((menu)->{
+                    menu.setChildren(getChildren(menu,entities));
+                    return menu;
+                }).sorted((menu1,menu2)->{
+                    return (menu1.getSort()==null?0:menu1.getSort()) - (menu2.getSort()==null?0:menu2.getSort());
+                        }).collect(Collectors.toList());
+
+
+                return children;
+    }
 }
